418d245c8557502779dfcf2e4d012e0a1da3d6de
David Meister <thedavidmeister@gmail.com>
2016-10-13 18:06:51 +1100

    brain dump

--- src/pivotal_tracker_clj/core.clj
+++ src/pivotal_tracker_clj/core.clj
@@ -2,15 +2,73 @@
 
 (def base-url "https://www.pivotaltracker.com/services/v5/")
 
+; This should be a protocol...
+(defn str->int [v] (if  (string? v)
+                        (Integer. (re-find #"\d+" v))
+                        (int v)))
+
+(defn number-of-pages
+  [limit pagination-total]
+  {:pre [(integer? limit) (integer? pagination-total)]}
+  (->
+    (/ pagination-total limit)
+    Math/ceil
+    int))
+
 (defn endpoint->url
   [endpoint]
   (str base-url endpoint))
+
+(def auth-headers {"X-TrackerToken" api-token})
+(defn with-auth-headers
+  [params key]
+  (update-in options [:headers] merge auth-headers))
+
+(def with-options
+  [params options]
+  (update-in options [:query-params] merge options))
+
+(defn get-page!
+  ([url] (get-page! url {}))
+  ([url options]
+   (let [options-a (with-auth-headers options)]
+    (http/get url options-a))))
+
+(defn api-error!
+  [headers]
+  (pprint/pprint headers)
+  (throw (Exception. "Failed response from Pivotal Tracker API. See headers above for debug info.")))
+
+; Options handling.
+
+(def page-limit 500)
+(defn pager-params
+  "Query parameters for page number X. Pages are 1 indexed."
+  [page-number]
+  {:limit page-limit
+    ; We have to dec the page number. E.g. "Page 1" is offset 0, etc.
+    :offset (* page-limit (dec page-number))})
+
+(defn with-pager-params
+  [options page-number]
+  (with-options (pager-params page-number) options))
+
+; Response dereffing.
+
+(defn response-body
+  [promise]
+  (walk/keywordize-keys (json/read-str (:body @promise))))
+
+(defn response-header
+  [promise]
+  (:headers @promise)))
 
 (defn get-all-pages!
   ([key endpoint] (get-all-pages! url {}))
   ([key endpoint options]
    (let [ url (endpoint->url endpoint)
-          options             (with-auth-headers options)
+          params {}
+          options             (-> {} (with-auth-headers key) (with-options options))
           first-request       (get-page! url (with-pager-params options 1))
           first-response      (response-body first-request)
           first-headers       (response-header first-request)
