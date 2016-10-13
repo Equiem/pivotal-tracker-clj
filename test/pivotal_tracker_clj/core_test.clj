70ad592b836e3ea04fe16d736596dfc5070766af
David Meister <thedavidmeister@gmail.com>
2016-10-13 17:00:19 +1100

    nitial commit

---
+++ test/pivotal_tracker_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns pivotal-tracker-clj.core-test
+  (:require [clojure.test :refer :all]
+            [pivotal-tracker-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
