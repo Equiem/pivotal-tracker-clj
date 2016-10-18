8f014ba9bc36718d9f8862b7011cd13402414ee6
David Meister <thedavidmeister@gmail.com>
2016-10-18 14:26:29 +1100

    comment out a test that doesn't do much

--- test/pivotal_tracker_clj/core_test.clj
+++ test/pivotal_tracker_clj/core_test.clj
@@ -2,8 +2,6 @@
   (:require [clojure.test :refer :all]
             [pivotal-tracker-clj.core :refer :all]))
 
-(deftest api-calls
-  (pivotal-tracker-clj.core/api!! ["projects" 493417 "stories"]
-                                  {:created_after "2016-01-01T00:00:00Z"}))
-                                  ; {:name "test"}))
-                                  ; :method :post))
+; (deftest api-calls
+;   (pivotal-tracker-clj.core/api!! ["projects" 493417 "stories"]
+;                                   {:created_after "2016-01-01T00:00:00Z"}))
