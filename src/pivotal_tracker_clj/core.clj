eff5819ce1ff449eb810469a37dcf515b9266e66
David Meister <thedavidmeister@gmail.com>
2016-10-18 13:28:04 +1100

    working pagination

--- src/pivotal_tracker_clj/core.clj
+++ src/pivotal_tracker_clj/core.clj
@@ -23,6 +23,12 @@
   [params url]
   (assoc params :url url))
 
+(defn with-page-number
+  [params page-number]
+  (let [page-size 500]
+    (with-options params {:limit page-size
+                          :offset (* page-size (dec page-number))})))
+
 (defn response-ok?
   [response]
   (= (and (-> response :status (= 200))
@@ -34,36 +40,16 @@
       :body
       (cheshire.core/parse-string true)))
 
+(defn scrub-params
+  [params]
+  (-> params
+      (assoc-in [:headers "X-TrackerToken"] "***HIDDEN**")))
+
 (defn scrub-response
   [response]
   (-> response
-      (assoc-in [:opts :headers "X-TrackerToken"] "***HIDDEN***")))
+      (update-in [:opts] scrub-params)))
 
-(defn api!
- ([token endpoint] (api! token endpoint {}))
- ([token endpoint options & params]
-  (taoensso.timbre/debug "Requesting Pivotal Tracker endpoint" endpoint)
-  (let [request (org.httpkit.client/request (-> (apply hash-map params)
-                                                (with-url (endpoint->url endpoint))
-                                                (with-options options)
-                                                (with-auth token)))
-        response @request]
-    (if (response-ok? response)
-        (response->data response)
-        (do
-          (taoensso.timbre/error (scrub-response response))
-          (throw (Exception. "Bad response from Pivotal Tracker")))))))
-
-(defn api-all-pages!
-  ([token endpoint] (api-all-pages! token endpoint {}))
-  ([token endpoint options & params]
-   ()))
-
-
-
-
-
-; This should be a protocol...
 (defn str->int [v] (if  (string? v)
                         (Integer. (re-find #"\d+" v))
                         (int v)))
@@ -76,65 +62,31 @@
     Math/ceil
     int))
 
-; (defn get-page!
-;   ([url] (get-page! url {}))
-;   ([url options]
-;    (let [options-a (with-auth options)]
-;     (http/get url options-a))))
+(defn get-page!
+  [params]
+  (taoensso.timbre/debug "Requesting Pivotal Tracker endpoint" (scrub-params params))
+  (let [request (org.httpkit.client/request params)
+        response @request]
+    (if (response-ok? response)
+        response
+        (do
+          (taoensso.timbre/error (scrub-response response))
+          (throw (Exception. "Bad response from Pivotal Tracker"))))))
 
-; (defn api-error!
-;   [headers]
-;   (pprint/pprint headers)
-;   (throw (Exception. "Failed response from Pivotal Tracker API. See headers above for debug info.")))
-
-; Options handling.
-
-; (def page-limit 500)
-; (defn pager-params
-;   "Query parameters for page number X. Pages are 1 indexed."
-;   [page-number]
-;   {:limit page-limit
-;     ; We have to dec the page number. E.g. "Page 1" is offset 0, etc.
-;     :offset (* page-limit (dec page-number))})
-;
-; (defn with-pager-params
-;   [options page-number]
-;   (with-options (pager-params page-number) options))
-;
-; ; Response dereffing.
-;
-; (defn response-body
-;   [promise]
-;   (walk/keywordize-keys (json/read-str (:body @promise))))
-;
-; (defn response-header
-;   [promise]
-;   (:headers @promise))
-;
-; (defn get-all-pages!
-;   ([token endpoint] (get-all-pages! url {}))
-;   ([token endpoint options]
-;    (let [ url (endpoint->url endpoint)
-;           params {}
-;           options             (-> {} (with-auth-headers key) (with-options options))
-;           first-request       (get-page! url (with-pager-params options 1))
-;           first-response      (response-body first-request)
-;           first-headers       (response-header first-request)
-;           first-error         (if-not (= "200 OK" (:status first-headers))
-;                                       (api-error! first-headers))
-;           ; Ultimately the page size is what PT will return, not what we ask for.
-;           page-size           (:x-tracker-pagination-returned first-headers)
-;           results-count       (:x-tracker-pagination-total first-headers)
-;           number-of-pages     (number-of-pages (str->int page-size) (str->int results-count))
-;           other-page-numbers  (range 2 (inc number-of-pages))
-;           other-requests      (if (< 1 number-of-pages)
-;                                   (vec (map #(get-page! url (with-pager-params options %))
-;                                             other-page-numbers)))
-;           other-responses     (vec (map response-body other-requests))
-;           other-headers       (vec (map response-header other-requests))
-;           all-responses       (into first-response other-responses)
-;           all-headers         (into [first-headers] other-headers)
-;           errors              (remove #(= "200 OK" (:status %))
-;                                       all-headers)]
-;       (if (not-empty errors) (api-error! errors))
-;       (vec (flatten all-responses)))))
+(defn api!
+ ([token endpoint] (api! token endpoint {}))
+ ([token endpoint options & params]
+  (let [base-params (-> (apply hash-map params)
+                        (with-url (endpoint->url endpoint))
+                        (with-options options)
+                        (with-auth token))
+        first-response (get-page! (with-page-number base-params 1))
+        page-size  (-> first-response :headers :x-tracker-pagination-returned)
+        results-count (-> first-response :headers :x-tracker-pagination-total)
+        number-of-pages     (number-of-pages (str->int page-size) (str->int results-count))
+        other-page-numbers  (range 2 (inc number-of-pages))
+        other-responses      (if  (< 1 number-of-pages)
+                                  (vec (pmap #(get-page! (with-page-number base-params %))
+                                              other-page-numbers)))
+        all-data (into (response->data first-response) (flatten (map response->data other-responses)))]
+    all-data)))
--- test/pivotal_tracker_clj/core_test.clj
+++ test/pivotal_tracker_clj/core_test.clj
@@ -2,7 +2,8 @@
   (:require [clojure.test :refer :all]
             [pivotal-tracker-clj.core :refer :all]))
 
-(deftest api-calls
-
-  (prn (pivotal-tracker-clj.core/api! (pivotal-tracker-clj.core/token)
-                                      ["me"])))
+; (deftest api-calls
+;   (-> (pivotal-tracker-clj.core/api!  (pivotal-tracker-clj.core/token)
+;                                       ["projects" 493417 "stories"])
+;       count
+;       prn))
