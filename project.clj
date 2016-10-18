45b92d3776eea0727da482264c7a39a7344d838f
David Meister <thedavidmeister@gmail.com>
2016-10-18 15:01:24 +1100

    updated git url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject pivotal-tracker-clj "0.1.0-SNAPSHOT"
   :description "Abstracts the Pivotal Tracker API a bit without implementing every endpoint."
-  :url "https://github.com/Equiem/pivotal-tracker-clj"
+  :url "https://github.com/thedavidmeister/pivotal-tracker-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [ [org.clojure/clojure "1.8.0"]
